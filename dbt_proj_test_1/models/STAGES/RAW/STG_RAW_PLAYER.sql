d88846235fcf600a448234761beeef5b52a339e3
raunit1610 <2106296@kiit.ac.in>
2025-11-10 05:32:48 +0000

    Incremental DBT Schema Implemented

--- dbt_proj_test_1/models/STAGES/RAW/STG_RAW_PLAYER.sql
+++ dbt_proj_test_1/models/STAGES/RAW/STG_RAW_PLAYER.sql
@@ -1,6 +1,8 @@
 {{ 
     config(
-        materialized='table'
+        materialized='incremental',
+        unique_key='PLAYERID',
+        incremental_strategy='merge'
     )
 }}
 
@@ -9,9 +11,22 @@ WITH source_data AS (
         {{ dbt_utils.star(from=source('t20_database', 'players')) }},
         CONVERT_TIMEZONE('UTC', CURRENT_TIMESTAMP())::TIMESTAMP_NTZ AS _inserted_at_
     FROM {{ source('t20_database', 'players') }}
+),
+
+deduped AS (
+    SELECT
+        PLAYERID,
+        ANY_VALUE(PLAYERNAME) AS PLAYERNAME,
+        ANY_VALUE(FILENAME) AS FILENAME,
+        ANY_VALUE(LOAD_TIMESTAMP) AS LOAD_TIMESTAMP,
+        MAX(_inserted_at_) AS _inserted_at_
+    FROM source_data
+    GROUP BY PLAYERID
 )
 
-
 SELECT *
-FROM source_data
+FROM deduped
 
+{% if is_incremental() %}
+  WHERE _inserted_at_ > (SELECT MAX(_inserted_at_) FROM {{ this }})
+{% endif %}
